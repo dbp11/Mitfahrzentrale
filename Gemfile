8a67adfe5f409cdfbb288d141e4fe2e89ee5178d
Nils Haldenwang <n.haldenwang@googlemail.com>
2011-07-19 10:40:56 +0200

    new Rails app generated by Rails Apps Composer gem

---
+++ Gemfile
@@ -0,0 +1,14 @@
+source 'http://rubygems.org'
+gem 'rails', '3.1.0.rc4'
+gem 'sqlite3'
+gem 'sass-rails', "~> 3.1.0.rc"
+gem 'coffee-script'
+gem 'uglifier'
+gem 'jquery-rails'
+gem "therubyracer", ">= 0.8.2"
+gem "haml", ">= 3.1.2"
+gem "haml-rails", ">= 0.3.4", :group => :development
+gem "rspec-rails", ">= 2.6.1", :group => [:development, :test]
+gem "factory_girl_rails", ">= 1.1.rc1", :group => :test
+gem "devise", ">= 1.4.2"
+gem "rails-footnotes", ">= 3.7", :group => :development
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mitfahrzentrale::Application.config.secret_token = '76f299364f40c38d66da1c13aeb5ca605af7d2431b3642f402a0855abc720d87d1b10a6ad89ae1913f6a9d3d749ff3a0980b0312d9b499df406d92277d5d0154'
