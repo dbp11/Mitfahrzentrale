061fd12f8099be77cb03b930699e9039fc7996fa
Christoph <cheichler@uos.de>
2011-07-27 17:03:39 +0200

    2. Tag Ende

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates :email, :uniqueness => true, :presence => true, :length => {:minimum => 1}
+
   # Setup accessible (or protected) attributes for your model
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me
 
@@ -15,5 +17,15 @@ class User < ActiveRecord::Base
 
  has_and_belongs_to_many :ignored, :class_name => "User", :join_table => "ignore", :foreign_key => "ignoring_id", :association_foreign_key => "ignored_id"
 
+  def to_s
+    name
+  end
 
+  def validate
+     if validate_uniqueness_of:email 
+       then "Alles Okay"
+       else  
+  end
+
+end
 end
