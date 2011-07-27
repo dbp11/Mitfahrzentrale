3d82e2bbf7cab6cc17c2a59fc860e89330b8ffad
sebrockm <sebrockm@uos.de>
2011-07-27 14:05:39 +0200

    kleinigkeiten

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,8 @@ class User < ActiveRecord::Base
   has_many :passengers
   has_and_belongs_to_many :requests
 
-  has_and_belongs_to_many :ignoring, class_name => "User", :join_table => "ignore", :foreign_key => "ignored_id", :association_foreign_key => "ignoring_id" 
+  has_and_belongs_to_many :ignoring, :class_name => "User", :join_table => "ignore", :foreign_key => "ignored_id", :association_foreign_key => "ignoring_id" 
 
- has_and_belongs_to_many :ignored, class_name => "User", :join_table => "ignore", :foreign_key => "ignoring_id", :association_foreign_key => "ignored_id"
+ has_and_belongs_to_many :ignored, :class_name => "User", :join_table => "ignore", :foreign_key => "ignoring_id", :association_foreign_key => "ignored_id"
 
 end
