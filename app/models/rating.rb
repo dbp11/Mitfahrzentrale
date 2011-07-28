140fe682aeb0d84cfc549fe4e489bff6560bf29d
Dominik Krümpelmann <dkruempe@uos.de>
2011-07-28 09:26:27 +0200

    Änderung rating model

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -2,7 +2,7 @@ class Rating < ActiveRecord::Base
   belongs_to :trip
   
   belongs_to :sent_to_id, :class_name =>"User"
-  belongs_to :written_by_id, :class_name =>"User",
+  belongs_to :written_by_id, :class_name =>"User"
 
   def to_s
     comment
