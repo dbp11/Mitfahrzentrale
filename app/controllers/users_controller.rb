169b7fed02c6ebc5bd5573938060370416903e1c
DBDominik <dstegman@uos.de>
2011-07-28 11:27:32 +0200

    UserController + Authorization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,20 @@ class UsersController < ApplicationController
 
   end
 
+  # GET /users/1/edit
+  def edit
+    @user = User.find(params[:id])
+  end
+
+
+  # PUT /users/1
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update_attributes(params[:trip])
+      redirect_to @user, notice: 'Trip was successfully updated.' 
+    else
+      render action: "edit" 
+    end
+  end
 end
