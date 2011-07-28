c8dc27e326df13775c09c71f0a359d70aedc332a
Christoph <cheichler@uos.de>
2011-07-28 10:25:50 +0200

    test

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,8 +1,9 @@
 class Rating < ActiveRecord::Base
-  belongs_to :trip
   
+  
+  belongs_to :trip
   belongs_to :rater, :class_name =>"User"
-  belongs_to :rated, :class_name =>"User",
+  belongs_to :rated, :class_name =>"User"
 
   def to_s
     comment
