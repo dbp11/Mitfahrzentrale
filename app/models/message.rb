eb017039b57e6d95771459b435d029e41d17ab6d
mibloemer <michael_bloemer@web>
2011-07-28 13:12:28 +0200

    bla

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,8 +1,4 @@
 class Message < ActiveRecord::Base
-<<<<<<< HEAD
-  has_many :writer, :class_name => "User"
-  has_many :receiver, :class_name => "User", :dependent => :destroy
-=======
    
    #Beziehung Message zu User als writer oder receiver
    belongs_to  :writer, :class_name => "User"
@@ -11,6 +7,6 @@ class Message < ActiveRecord::Base
    def to_s
      writer+ " an " +receiver+ ": " +message
    end
->>>>>>> 1625150ca54459fa875764906538b275c03addef
+
 
 end
