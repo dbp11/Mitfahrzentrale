49cf06a6858c02314b8c09b5a642a7fba45d27f1
Dominik Krümpelmann <dkruempe@uos.de>
2011-07-28 10:22:59 +0200

    ratings

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -3,7 +3,7 @@ class Rating < ActiveRecord::Base
   
   belongs_to :trip
   belongs_to :rater, :class_name =>"User"
-  belongs_to :rated, :class_name =>"User",
+  belongs_to :rated, :class_name =>"User"
 
   def to_s
     comment
