41650464de0af60e7ea11a59c7f1df0f010c1b96
pschnetg <patrick.schnetger@gmx.de>
2011-07-29 11:46:49 +0200

    cars

--- db/schema.rb
+++ db/schema.rb
@@ -65,21 +65,8 @@ ActiveRecord::Schema.define(:version => 20110729074702) do
 
   add_index "ratings", ["trip_id"], :name => "index_ratings_on_trip_id"
 
-  create_table "requests", :force => true do |t|
-    t.float    "starts_at_N"
-    t.float    "starts_at_E"
-    t.float    "ends_at_N"
-    t.float    "ends_at_E"
-    t.string   "address_start"
-    t.string   "address_end"
-    t.datetime "start_time"
-    t.datetime "end_time"
-    t.boolean  "baggage"
-    t.string   "comment"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "user_id"
-  end
+# Could not dump table "requests" because of following StandardError
+#   Unknown type 'user' for column 'user'
 
   create_table "trips", :force => true do |t|
     t.integer  "user_id"
