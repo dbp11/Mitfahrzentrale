4b1e3721040a7828401cc347ac01a4e255c3e490
Dominik Krümpelmann <dkruempe@uos.de>
2011-07-29 13:10:17 +0200

    Test Cars

--- spec/models/car_spec.rb
+++ spec/models/car_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe Car do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @attr = {
+      :user_id => 45645
+      :seats => 5
+      :trunk => "AHA"
+      :licence => 1231
+      :fuel_consumption => 5.5
+      :smoker => true
+      :description => "AHA"'
+    }
+  end
 end
