3209050e57966bff8cd2a07f57535db7998ded37
sebrockm <sebrockm@uos.de>
2011-07-29 16:29:46 +0200

    bugfix

--- app/models/user.rb
+++ app/models/user.rb
@@ -40,7 +40,7 @@ class User < ActiveRecord::Base
   def driven
    erg=[] 
    driver_trips.each do |x|
-     if x.end_time < Time.now
+     if x.start_time < Time.now
        then erg = erg << x
      end
    end
@@ -51,7 +51,7 @@ class User < ActiveRecord::Base
   def to_drive
     erg=[]
     driver_trips.each do |x|
-      if x.end_time > Time.now
+      if x.start_time > Time.now
         then erg = erg << x
       end
     end
@@ -62,7 +62,7 @@ class User < ActiveRecord::Base
   def driven_with
     erg=[]
     passenger_trips.each do |x|
-      if x.end_time < Time.now
+      if x.start_time < Time.now
         then erg = erg << x
       end
     end
@@ -73,7 +73,7 @@ class User < ActiveRecord::Base
   def to_drive_with
     erg=[]
     passenger_trips.each do |x|
-      if x.end_time > Time.now
+      if x.start_time > Time.now
         then erg = erg << x
       end
     end
