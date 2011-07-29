f8cddec13fe29115201a5a914dd493b5b4cdf8bc
sebrockm <sebrockm@uos.de>
2011-07-29 11:30:49 +0200

    Zeitintervalle berücksichtigt bei get_similar_trips/requests

--- app/models/request.rb
+++ app/models/request.rb
@@ -3,15 +3,28 @@ class Request < ActiveRecord::Base
   #Beziehungen
   belongs_to :user
 
+  #Berechnet Trips, die sich nur geringfügig von dieser Request unterscheiden, und gibt ein Array aus 
+  #Wertepaaren zurück. Der erste Wert ist die trip_id, der zweite gibt die Länge des Umweges an, den der 
+  #Fahrer dieses Trips in Kauf nehmen müsste. Das Array ist absteigend nach Umwegen sortiert.
   def get_similar_trips
-    distance = distance_between [starts_at_N, starts_at_E] [ends_at_N, ends_at_E] {:km}
+    distance = Geocoder::Calculations.distance_between [starts_at_N, starts_at_E], 
+                                                       [ends_at_N, ends_at_E], :units => :km
+    start_f = start_time.to_f
+    end_f = end_time.to_f
     erg = {}
-    @trips = Trips.all.each do |t|
-      distance_start = distance_between [t.starts_at_N, t.starts_at_E] [starts_at_N, starts_at_E] {:km}
-      distance_end =  distance_between [t.ends_at_N, t.ends_at_E] [ends_at_N, ends_at_E] {:km}
-      distance_t =  distance_between [t.starts_at_N, t.start_at_E] [t.ends_at_N, t.ends_at_E] {:km}
-      difference = distance + distance_start + distance_end - distance_t
-      erg[t.id] = difference
+
+    Trips.all.each do |t|
+      t_start_f = t.start_time.to_f
+      if t_start_f.between?(start_f, end_f) then  
+        distance_start = Geocoder::Calculations.distance_between [t.starts_at_N, t.starts_at_E], 
+                                                                 [starts_at_N, starts_at_E], :units => :km
+        distance_end =  Geocoder::Calculations.distance_between [t.ends_at_N, t.ends_at_E], 
+                                                                [ends_at_N, ends_at_E], :units => :km
+        distance_t =  Geocoder::Calculations.distance_between [t.starts_at_N, t.start_at_E], 
+                                                              [t.ends_at_N, t.ends_at_E], :units => :km
+        difference = distance + distance_start + distance_end - distance_t
+        erg[t.id] = difference
+      end
     end
 
     erg.sort{|a,b| a[1] <=> b[1]}
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -13,15 +13,28 @@ class Trip < ActiveRecord::Base
     id 
   end
 
+  #Brechnet Requests, die sich nur geringfügig von diesem Trip unterscheiden, und gibt ein Array aus
+  #Wertepaaren zurück. Der erste Wert ist die request_id, der zweite gibt die Länge des Umweges an, den der 
+  #Fahrer dieses Trips in Kauf nehmen müsste. Das Array ist absteigend nach Umwegen sortiert.
   def get_similar_requests
-    distance = distance_between [starts_at_N, starts_at_E] [ends_at_N, ends_at_E] {:km}
+    distance = Geocoder::Calculations.distance_between [starts_at_N, starts_at_E], 
+                                                       [ends_at_N, ends_at_E], :units => :km
+    start_f = start_time.to_f
     erg = {}
-    @trips = Requests.all.each do |t|
-      distance_start = distance_between [t.starts_at_N, t.starts_at_E] [starts_at_N, starts_at_E] {:km}
-      distance_end =  distance_between [t.ends_at_N, t.ends_at_E] [ends_at_N, ends_at_E] {:km}
-      distance_t =  distance_between [t.starts_at_N, t.start_at_E] [t.ends_at_N, t.ends_at_E] {:km}
-      difference = distance_t + distance_start + distance_end - distance
-      erg[t.id] = difference
+
+    Requests.all.each do |r|
+      r_start_f = r.start_time.to_f
+      r_end_f = r.end_time.to_f
+      if start_f.between?(r_start_f, r_end_f) then  
+        distance_start = Geocoder::Calculations.distance_between [r.starts_at_N, r.starts_at_E], 
+                                                                 [starts_at_N, starts_at_E], :units => :km
+        distance_end =  Geocoder::Calculations.distance_between [r.ends_at_N, r.ends_at_E], 
+                                                                [ends_at_N, ends_at_E], :units => :km
+        distance_r =  Geocoder::Calculations.distance_between [r.starts_at_N, r.start_at_E], 
+                                                              [r.ends_at_N, r.ends_at_E], :units => :km
+        difference = distance_r + distance_start + distance_end - distance
+        erg[r.id] = difference
+      end
     end
 
     erg.sort{|a,b| a[1] <=> b[1]}
