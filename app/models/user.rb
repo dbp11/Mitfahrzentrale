a9da4120ed510df74650225513025562a2fa9025
Dominik Krümpelmann <dkruempe@uos.de>
2011-07-29 15:48:02 +0200

    Test Files Erstellt ohne Beziehungen

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
   validates_presence_of :name, :address, :zipcode, :city, :birthday 
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :name, :email, :password, :password_confirmation, :remember_me
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :address, :zipcode, :birthday, :city
   
   has_attached_file :picture, :styles => { :medium =>  "300x300>", :thumb => "100x100>"}
 
