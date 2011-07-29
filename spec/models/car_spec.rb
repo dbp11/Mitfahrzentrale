2a67f3699f2b2fadea65296d9b865df6fe515b76
Dominik Krümpelmann <dkruempe@uos.de>
2011-07-29 13:32:04 +0200

    Car Test

--- spec/models/car_spec.rb
+++ spec/models/car_spec.rb
@@ -3,13 +3,12 @@ require 'spec_helper'
 describe Car do
   before(:each) do
     @attr = {
-      :user_id => 45645
-      :seats => 5
-      :trunk => "AHA"
-      :licence => 1231
-      :fuel_consumption => 5.5
-      :smoker => true
-      :description => "AHA"'
+      :user_id => 45645,
+      :seats => 5,
+      :licence => 1231,
+      :fuel_consumption => 5.5,
+      :smoker => true,
+      :description => "AHA",
     }
   end
 end
