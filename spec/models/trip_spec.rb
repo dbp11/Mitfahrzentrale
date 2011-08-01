07f6c440e8a39cea1a62ae88cb3563024d988418
Dominik Krümpelmann <dkruempe@uos.de>
2011-08-01 12:01:43 +0200

    Test Trip-Basis

--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -1,5 +1,20 @@
 require 'spec_helper'
 
 describe Trip do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @attr = {
+      :user_id => 4564,
+      :car_id => 545,
+      :starts_at_N => 12,
+      :starts_at_E => 35,
+      :ends_at_E => 45,
+      :ends_at_N => 5,
+      :address_start => "grrggrdrg",
+      :address_end => "fsefsefffse",
+      :start_time => Time.now,
+      :comment => "fejskfsbnfjkfesfse",
+      :baggage => true,
+      :free_seats => 4
+    }
+  end
 end
