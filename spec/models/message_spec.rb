92e5d217a9ae5025530a642b0406644b0b030d89
Dominik Krümpelmann <dkruempe@uos.de>
2011-08-02 11:39:39 +0200

    Test und Korrektur

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 require 'spec_helper'
 
 describe Message do
@@ -7,9 +8,23 @@ describe Message do
       :writer_id => 123,
       :receiver_id => 321,
       :delete_writer => false,
-      :delte_receiver => false
+      :delete_receiver => false
     }
   end
 
   #Test ob Validation funktionieren
+  it "Kontrolle der Beschränkung true,true" do
+    no_right_delete = Message.new(@attr.merge(:delete_writer => true, :delete_receiver => true))
+    no_right_delete.should_not be_valid
+  end
+
+  it "Kontrolle der Validation delete_receiver" do
+    no_delete_receiver = Message.new(@attr.merge(:delete_receiver => nil))
+    no_delete_receiver.should_not be_valid
+  end
+
+  it "Kontrolle der Validation delete_writer" do
+    no_delete_writer = Message.new(@attr.merge(:delelte_writer => nil))
+    no_delete_writer.should_not be_valid
+  end
 end
