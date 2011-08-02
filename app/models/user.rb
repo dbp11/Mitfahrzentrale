8ce0501772434764778fdfbf29bc8b35ff43d99e
sebrockm <sebrockm@uos.de>
2011-08-02 10:28:29 +0200

    bug..

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,34 +1,26 @@
-# Modellierung der User-Entität mit seinen Datenfeldern zuzüglich           
-
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   #Validations:
-  # Stat. Integrität: Email muss vorhanden, einzigartig und min 8 char lang sein
+  # Stat. Integrität: Email muss vorhanden, unique und min 8 char lang sein
   validates :email, :uniqueness => true, :presence => true, :length => {:minimum => 8}
-  #Vermeidung von Nullwerten bei Name, Adresse, Plz, Stadt
+  #Vermeidung von Nullwerten 
   validates_presence_of :name, :address, :zipcode, :city 
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :address, :zipcode, :birthday, :city, :sex, :phone, :instantmessenger, :visible_age, :visible_address, :visible_zip, :visible_phone, :visible_city, :visible_im, :visible_email, :visible_cars, :roles
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :address, :zipcode, :birthday, :city, :sex, :phone, :instantmessenger, :visible_age, :visible_address, :visible_zip, :visible_phone, :visible_city, :visible_im, :visible_email, :visible_cars
   
   
   #Von Paperclip gefordertes Statement zum Anhängen von Bildern
   has_attached_file :picture, :styles => { :medium =>  "300x300>", :thumb => "100x100>"}
 
-  #Beziehungen: 
-  #
-  #Join-Statement um über die Tabelle Passengers einen oder mehrere User mit einem Trip in Verbindung zu setzen. Die in der Tabelle Passengers gelisteten Userentitäten sind Mitfahrer
+  #Beziehungen:
   has_many :passenger_trips, :class_name => "Trip", :through => :passengers, :source => :trip 
-  #Beziehung zwischen Usern und Trips. Hierbei sind die User fahrer
   has_many :driver_trips, :class_name => "Trip", :foreign_key => "user_id"
-  #1 User hat n Cars
   has_many :cars, :dependent => :destroy
-  #1 User kann bei n Trips Mitfahrer gewesen sein
   has_many :passengers, :dependent => :destroy 
-  #1 User kann viele Mitfahrgesuche haben
   has_many :requests, :dependent => :destroy
 
  
@@ -36,24 +28,19 @@ class User < ActiveRecord::Base
   has_and_belongs_to_many :ignorings, :class_name => "User", :join_table => "ignore", :foreign_key => "ignored_id", :association_foreign_key => "ignoring_id"  
   has_and_belongs_to_many :ignoreds, :class_name => "User", :join_table => "ignore", :foreign_key => "ignoring_id", :association_foreign_key => "ignored_id"
   
-  #Beziehung User schreibt n Usern Nachrichten/Ratings und bekommt von mehreren Usern Nachrichten und Bewertungen
+  #Beziehung User schreibt User Nachricht/Rating
   has_many :received_messages, :class_name => "Message", :foreign_key =>"receiver_id", :dependent => :destroy
   has_many :written_messages,  :class_name => "Message", :foreign_key =>"writer_id", :dependent => :destroy
   has_many :written_ratings, :class_name => "Rating", :foreign_key => "author_id", :dependent => :destroy
   has_many :received_ratings, :class_name => "Rating", :foreign_key => "receiver_id", :dependent => :destroy
-
-  ROLES = %w[admin member]
-
+ 
   #Methoden:
   #toString Methode für User
-  #@return string name
   def to_s
     name
   end
-
-
+  
   #Vergangene angebotene Trips des Users
-  #@return Trip [] erg
   def driven
    erg=[] 
    driver_trips.each do |x|
@@ -65,7 +52,6 @@ class User < ActiveRecord::Base
   end
 
   #Noch nicht vergangene angebotene Trips des Users
-  #@return Trip [] erg
   def to_drive
     erg=[]
     driver_trips.each do |x|
@@ -77,7 +63,6 @@ class User < ActiveRecord::Base
   end
 
   #Vergangene Suchen des Users
-  #@return Trip [] er 
   def driven_with
     erg=[]
     passenger_trips.each do |x|
@@ -89,7 +74,6 @@ class User < ActiveRecord::Base
   end
 
   #Noch laufende Suchen des Users
-  #@return Trip [] erg
   def to_drive_with
     erg=[]
     passenger_trips.each do |x|
@@ -99,9 +83,7 @@ class User < ActiveRecord::Base
     end
     return erg
   end
-  
-  #Methode zur Ermittlung der durchschnittlichen Bewertung des Users
-  #@return float erg/count auf 2 Nachkommastellen gerundet.
+
   def get_avg_rating
     count = 0
     erg = 0
@@ -109,11 +91,9 @@ class User < ActiveRecord::Base
       erg = erg + x.mark
       count +=1
     end
-    return (erg / count).round(2)
+    return erg / count
   end
 
-  # Methode zum Zählen der erhaltenen Ratings
-  # @return integer count
   def count_ratings
     count = 0
     self.received_ratings.each do |x|
@@ -125,7 +105,7 @@ class User < ActiveRecord::Base
   def get_written_messages
     erg = []
     self.written_messages.each do |m|
-      if m.delete_writer? then
+      if !m.delete_writer? then
         erg << m
       end
     end
@@ -133,11 +113,12 @@ class User < ActiveRecord::Base
   end
 
   def get_received_messages
-    eg = []
+    erg = []
     self.received_messages.each do |m|
-      if m.delete_receiver? then
+      if !m.delete_receiver? then
         erg << m
       end
     end
+    return erg
   end
 end
