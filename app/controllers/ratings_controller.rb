807ca6dbdcb257f7fbe8d25675271011e39e7ca0
rbuss <rbuss@uos.de>
2011-08-03 15:59:28 +0200

    view and controler for new rating

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -26,6 +26,8 @@ class RatingsController < ApplicationController
   # GET /ratings/new.json
   def new
     @rating = Rating.new
+	@rating.receiver = User.find(params[:uid])
+	@rating.trip = Trip.find(params[:tid])
 
     respond_to do |format|
       format.html # new.html.erb
@@ -42,16 +44,16 @@ class RatingsController < ApplicationController
   # POST /ratings.json
   def create
     @rating = Rating.new(params[:rating])
-
     respond_to do |format|
-      if @rating.save
-        format.html { redirect_to @rating, notice: 'Rating was successfully created.' }
+	  if @rating.save
+        format.html { redirect_to ratings_path, notice: 'Rating was successfully created.' }
         format.json { render json: @rating, status: :created, location: @rating }
       else
         format.html { render action: "new" }
         format.json { render json: @rating.errors, status: :unprocessable_entity }
       end
     end
+	puts "########################################### 59"
   end
 
   # PUT /ratings/1
